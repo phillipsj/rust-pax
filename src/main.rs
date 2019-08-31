ec6dcced328796b7fe7b488e2c6e56365bc5696d
Jamie Phillips <cphill11@gmail.com>
2019-08-30 22:19:37 -0400

    It compiles.

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,73 @@
+use std::fs;
 use std::fs::File;
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 
 enum Folder {
     File(String),
-    Folder(String, Vec<Folder>)
+    Folder(String, Vec<Folder>),
 }
 
-fn strings_to_files(file_names: &[&str]) -> Vec<Folder> {
-    file_names.into_iter().map(|name| Folder::File(name.to_string())).collect()
+fn convert_strings_to_files(file_names: &[&str]) -> Vec<Folder> {
+    file_names
+        .into_iter()
+        .map(|name| Folder::File(name.to_string()))
+        .collect()
 }
 
-fn create_envs_folder() -> Folder {
-    let env_files = strings_to_files(&["dev.tfvars", "qa.tfvars", "prod.tfvars"]);
+fn generate_envs_folder() -> Folder {
+    let env_files = convert_strings_to_files(&["dev.tfvars", "qa.tfvars", "prod.tfvars"]);
     Folder::Folder(String::from("envs"), env_files)
 }
 
-fn create_service_folder(name: &str) -> Folder {
-    let envs_folder = create_envs_folder();
-    let files = strings_to_files(&["main.tf", "main.tf", "output.tf"]);
+fn generate_service_folder(name: &str) -> Folder {
+    let envs_folder = generate_envs_folder();
+    let mut files = convert_strings_to_files(&["main.tf", "main.tf", "output.tf"]);
+    files.push(envs_folder);
     Folder::Folder(name.to_string(), files)
 }
 
-fn create_infrastructure_folder(app_name: &str) -> Folder {
-    let service_folder = create_service_folder(app_name);
+fn generate_infrastructure_folder(app_name: &str) -> Folder {
+    let service_folder = generate_service_folder(app_name);
     Folder::Folder("infrastructure".to_string(), vec![service_folder])
 }
 
-fn create_paths(filesystem: Folder) -> PathBuf {
-    let stuff = match filesystem {
-        Folder::File(x) => PathBuf::from(x),
+fn generate_paths(filesystem: Folder) -> Vec<PathBuf> {
+    let file_paths = match filesystem {
+        Folder::File(x) => vec![PathBuf::from(x)],
         // After the first map I have a list of paths that I now need to append the folder to the front
-        Folder::Folder(folder, folders) => folders.into_iter().map(|name| create_paths(name)).map(|path| PathBuf::from(&folder).join(path)).collect()
+        Folder::Folder(folder, folders) => folders
+            .into_iter()
+            .map(|name| generate_paths(name))
+            .into_iter()
+            .map(|paths| {
+                paths
+                    .into_iter()
+                    .map(|path| PathBuf::from(&folder).join(path))
+                    .collect()
+            })
+            .collect(),
     };
-    stuff
+    file_paths
+}
+
+fn create_path(path: PathBuf) -> std::io::Result<()> {
+    if let Some(parent) = path.parent() {
+        fs::create_dir_all(parent)?
+    }
+    File::create(path)?;
+    Ok(())
 }
 
 fn main() -> std::io::Result<()> {
-    let filesystem = create_infrastructure_folder("myapp");
-    let paths = create_paths(filesystem);
-    let create = File::create("test/filename.txt")?;
-    println!("Hello, world!");
+    let filesystem = generate_infrastructure_folder("myapp");
+    let paths = generate_paths(filesystem);
+    for path in paths {
+        if let Some(stuff) = path.to_str() {
+            println!("{}",stuff);
+        }
+
+        create_path(path)?;
+    }
 
     Ok(())
 }
