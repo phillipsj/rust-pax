c0db2d93336ae95142b6dd7b36ba4b57fa0b4b81
Jamie Phillips <cphill11@gmail.com>
2019-09-01 22:00:36 -0400

    Adding command line args and parsing.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use clap::{App, AppSettings, Arg, SubCommand};
 use std::fs;
 use std::fs::File;
 use std::path::PathBuf;
@@ -31,6 +32,11 @@ fn generate_infrastructure_folder(app_name: &str) -> Folder {
     Folder::Folder("infrastructure".to_string(), vec![service_folder])
 }
 
+fn generate_module_folder(name: &str) -> Folder {
+    let files = convert_strings_to_files(&["main.tf", "variables.tf", "output.tf"]);
+    Folder::Folder(name.to_string(), files)
+}
+
 fn generate_paths(filesystem: Folder) -> Vec<PathBuf> {
     let file_paths = match filesystem {
         Folder::File(x) => vec![PathBuf::from(x)],
@@ -54,10 +60,127 @@ fn create_path(path: PathBuf) -> std::io::Result<()> {
 }
 
 fn main() -> std::io::Result<()> {
-    let filesystem = generate_infrastructure_folder("myapp");
-    let paths = generate_paths(filesystem);
-    for path in paths {
-        create_path(path)?;
+    let matches = App::new("pax")
+        .about("A tool that aids working with DevOps tooling, specifically Terraform and Packer.")
+        .version("1.0")
+        .author("BlueGhost Labs - Jamie Phillips")
+        .subcommand(SubCommand::with_name("tf")
+            .about("Terraform related commands")
+            .setting(AppSettings::SubcommandRequiredElseHelp)
+            .subcommand(SubCommand::with_name("new") 
+                .about("Creates new directories for Terraform.")
+                .subcommand(SubCommand::with_name("infra")
+                .about("Creates the entire infrastructure directory.")
+                .arg(Arg::with_name("name")
+                    .required(false)
+                    .help("The name for the application. If not provided defaults to app.")))
+                .subcommand(SubCommand::with_name("app")
+                .about("Creates a new application in the current directory.")
+                .arg(Arg::with_name("name")
+                    .required(true)
+                    .help("The name for the application.")))
+                .subcommand(SubCommand::with_name("module")
+                .about("Creates a new module in the current directory.")
+                .arg(Arg::with_name("name")
+                    .required(true)
+                    .help("The name of the module."))))
+            .subcommand(SubCommand::with_name("logging")
+                .about("Enables logging for Terraform.")
+                .setting(AppSettings::ArgRequiredElseHelp)
+                .subcommand(SubCommand::with_name("enable")
+                    .about("Enables logging.")                
+                    .arg(Arg::with_name("file")
+                        .required(false)
+                        .default_value("terraform_log.txt")
+                        .help("Defines a custom log file name. Default will be terraform_log.txt")))
+                .subcommand(SubCommand::with_name("disable")
+                    .about("Disables logging."))))
+        .subcommand(SubCommand::with_name("pk")
+            .about("Packer related commands")
+            .setting(AppSettings::SubcommandRequiredElseHelp)
+            .subcommand(SubCommand::with_name("new") 
+                .about("Creates new project for Packer.")
+                .arg(Arg::with_name("name")
+                    .required(true)
+                    .help("The name for the Packer project.")))
+            .subcommand(SubCommand::with_name("logging")
+                .about("Control logging for Packer.")
+                .setting(AppSettings::ArgRequiredElseHelp)
+                .subcommand(SubCommand::with_name("enable")
+                    .about("Enables logging.")                
+                    .arg(Arg::with_name("file")
+                        .required(false)
+                        .default_value("packer_log.txt")
+                        .help("Defines a custom log file name. Default will be packer_log.txt")))
+                .subcommand(SubCommand::with_name("disable")
+                    .about("Disables logging."))))
+        .get_matches();
+
+    match matches.subcommand() {
+        ("tf", Some(tf_matches)) => match tf_matches.subcommand() {
+            ("new", Some(new_matches)) => match new_matches.subcommand() {
+                ("infra", Some(infra_matches)) => {
+                    let filesystem =
+                        generate_infrastructure_folder(infra_matches.value_of("name").unwrap());
+                    let paths = generate_paths(filesystem);
+                    for path in paths {
+                        create_path(path)?;
+                    }
+                }
+                ("app", Some(app_matches)) => {
+                    let filesystem = generate_service_folder(app_matches.value_of("name").unwrap());
+                    let paths = generate_paths(filesystem);
+                    for path in paths {
+                        create_path(path)?;
+                    }
+                }
+                ("module", Some(module_matches)) => {
+                    let filesystem =
+                        generate_module_folder(module_matches.value_of("name").unwrap());
+                    let paths = generate_paths(filesystem);
+                    for path in paths {
+                        create_path(path)?;
+                    }
+                }
+                _ => unreachable!(),
+            },
+            ("logging", Some(logging_matches)) => match logging_matches.subcommand() {
+                ("enable", Some(enable_matches)) => {
+                    println!(
+                        "Enabling logging for Terraform with file name: {}",
+                        enable_matches.value_of("file").unwrap()
+                    );
+                }
+                ("disable", Some(_)) => {
+                    println!("Disabling logging for Terraform.");
+                }
+                _ => unreachable!(),
+            },
+            _ => unreachable!(),
+        },
+        ("pk", Some(pk_matches)) => match pk_matches.subcommand() {
+            ("new", Some(new_matches)) => {
+                println!(
+                    "Creating new Packer project {}",
+                    new_matches.value_of("name").unwrap()
+                );
+            }
+            ("logging", Some(logging_matches)) => match logging_matches.subcommand() {
+                ("enable", Some(enable_matches)) => {
+                    println!(
+                        "Enabling logging for Packer with file name: {}",
+                        enable_matches.value_of("file").unwrap()
+                    );
+                }
+                ("disable", Some(_)) => {
+                    println!("Disabling logging for Packer.");
+                }
+                _ => unreachable!(),
+            },
+            _ => unreachable!(),
+        },
+        ("", None) => println!("No subcommand was used"),
+        _ => unreachable!(),
     }
 
     Ok(())
